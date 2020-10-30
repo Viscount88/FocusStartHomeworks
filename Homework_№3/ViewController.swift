06ce8bbad012f674f23d35ea2974523b74b2667b
Lumkooo <shaminandrey2013@gmail.com>
2020-10-30 09:44:01 +0700

    Создание пустого проекта в workspace

---
+++ Homework_№3/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Homework_№3
+//
+//  Created by Андрей Шамин on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
